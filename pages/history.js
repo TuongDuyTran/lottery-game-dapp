edf85f32998d8b7ab76d7c27d6b3bc3a5ea92035
long <pdlong578@gmail.com>
2022-06-30 16:30:38 +0700

    handle history

--- pages/history.js
+++ pages/history.js
@@ -1,24 +1,82 @@
 import styles from "../styles/Home.module.css";
 import "antd/dist/antd.css";
-import { Button, PageHeader, Image, Table, Row, Col } from "antd";
+import { Button, PageHeader, Table } from "antd";
 import { HomeOutlined, WalletOutlined } from "@ant-design/icons";
 import { useRouter } from "next/router";
 import { Footer } from "antd/lib/layout/layout";
+import DataContext from "../context/dataContext";
+import { useState, useContext, useEffect } from "react";
+import Web3 from "web3";
+import initializeLotteryContract from "../blockchain/lottery";
 
 export default function History() {
   const router = useRouter();
-  const dataSource = [
-    {
-      key: "1",
-      name: "User2",
-      address: "0XAb",
-    },
-    {
-      key: "2",
-      name: "User1",
-      address: "0XCD",
-    },
-  ];
+  const [data, set_Data] = useContext(DataContext);
+  const [web3, setWeb3] = useState();
+  const [lotteryContract, set_LotteryContract] = useState();
+  const [address, setAddress] = useState();
+  const [error, set_error] = useState();
+  const [successMsg, set_successMsg] = useState();
+  const [lotteryHistory, set_LotteryHistory] = useState([]);
+  const [lotteryId, set_LotteryId] = useState();
+
+  useEffect(() => {
+    if (lotteryContract || data.contract) {
+      if(!lotteryContract) {
+        set_LotteryContract(data.contract);
+      }
+      if(lotteryContract){
+        getLotteryId();
+        getHistory(lotteryId);
+      }
+    }
+    if(error) {
+      handleError();
+    }
+      
+  },[lotteryContract, lotteryId]);
+
+  const handleConnectWallet = async () => {
+    if (
+      typeof window !== "undefined" &&
+      typeof window.ethereum !== "undefined"
+    ) {
+      try {
+        await window.ethereum.request({ method: "eth_requestAccounts" });
+        const web3 = new Web3(window.ethereum);
+        setWeb3(web3);
+        const accounts = await web3.eth.getAccounts();
+        setAddress(accounts[0]);
+
+        const lc = initializeLotteryContract(web3);
+        set_LotteryContract(lc);
+        set_Data({contract: lc});
+      } catch (err) {
+        set_error(err.message);
+        console.log(
+          "🚀 ~ file: index.js ~ line 20 ~ handleConnectWal ~ err",
+          err.message
+        );
+      }
+    } else {
+      console.log(
+        "🚀 ~ file: index.js ~ line 23 ~ handleConnectWal ~ err",
+        "MetaMask not found"
+      );
+    }
+  };
+
+  const getHistory = async (id) => {
+    for (let i = parseInt(id); i > 0 ; i--) {
+      const winner = await lotteryContract.methods.lotteryHistory(i).call();
+      set_LotteryHistory([...lotteryHistory, {key: id-i+1, name: `https://etherscan.io/address/${winner}`, address: winner}])
+    }
+  }
+
+  const getLotteryId = async() => {
+    const lotteryId = await lotteryContract.methods.lotteryId().call();
+    set_LotteryId(lotteryId);
+  }
 
   const columns = [
     {
@@ -55,7 +113,7 @@ export default function History() {
             <HomeOutlined />
             Trang chủ
           </Button>,
-          <Button key="1" type="primary" shape="round">
+          <Button key="1" type="primary" shape="round" onClick={handleConnectWallet}>
             <WalletOutlined />
             Kết nối Wallet
           </Button>,
@@ -65,7 +123,7 @@ export default function History() {
         }}
       ></PageHeader>
       <div className={styles.headerTitle}>Lịch sử người chiến thắng</div>
-      <Table dataSource={dataSource} columns={columns} />;
+      <Table dataSource={lotteryHistory} columns={columns} />;
       <Footer className={styles.footerPage}>
 
         <div className={styles.copyRight}>
